1c2a55e4b47a6bd4521eaedcd8c13fae3669c21c
Kaur Järvpõld <kaur@comodule.com>
2021-02-07 23:35:59 +0200

    add tanify logo

--- src/screens/landingScreen/sunScreen.tsx
+++ src/screens/landingScreen/sunScreen.tsx
@@ -1,37 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, ScrollView, View } from 'react-native';
 import { getWeatherData, WeatherData } from '../../services/weatherService';
+import TanifyLogo from '../../assets/tanify.svg';
 
 const SunScreen = () => {
-    const [weatherData, setWeatherData] = useState<WeatherData>(undefined);
-
-    useEffect(() => {
-        getWeatherData()
-            .then((data) => {
-                setWeatherData(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const [weatherData, setWeatherData] = useState<WeatherData>({
+        uvIndex: 0,
+        temperature: -5,
     });
 
+    // useEffect(() => {
+    //     getWeatherData()
+    //         .then((data) => {
+    //             setWeatherData(data);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }, []);
+
     return (
-        <>
-            <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <ScrollView
+                style={styles.scrollView}
+                contentContainerStyle={styles.contentContainer}>
+                <TanifyLogo
+                    // height={100}
+                    // width={'300'}
+                    style={{ borderWidth: 3 }}
+                />
                 <Text>{weatherData?.uvIndex}</Text>
                 <Text>{weatherData?.temperature}</Text>
-            </View>
-        </>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        ...StyleSheet.absoluteFillObject,
-        flexDirection: 'column',
-        justifyContent: 'center',
+        flex: 1,
+    },
+    scrollView: {
+        backgroundColor: '#3d8bdd',
+    },
+    contentContainer: {
         alignItems: 'center',
-        backgroundColor: 'white',
     },
 });
 
