30cdc013da7e21a0091e1509ddae9095be7a7937
Kaur Järvpõld <kaur@comodule.com>
2021-03-15 22:29:21 +0200

    style adjustments

--- src/screens/sunScreen/index.tsx
+++ src/screens/sunScreen/index.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Text, StyleSheet, ScrollView, View, Image } from 'react-native';
+import {
+    Text,
+    StyleSheet,
+    ScrollView,
+    View,
+    Image,
+    Dimensions,
+} from 'react-native';
 import { getWeatherData, WeatherData } from '../../services/weatherService';
 import { TanifyLogo } from '../../assets';
 import CircleView from '../../components/circleView';
@@ -10,6 +17,10 @@ const SunScreen = () => {
         uvIndex: 0,
         temperature: undefined,
     });
+    const windowHeight = Dimensions.get('window').height;
+    const logoWidthHeightRatio = 2.729;
+    const logoHeight = windowHeight * 0.17;
+    const logoWidth = logoHeight * logoWidthHeightRatio;
 
     useEffect(() => {
         getWeatherData()
@@ -25,40 +36,52 @@ const SunScreen = () => {
         <ScrollView
             style={styles.scrollView}
             contentContainerStyle={styles.contentContainer}>
-            <View style={styles.tanifyLogoContainer}>
-                <Image source={TanifyLogo} style={styles.tanifyLogo} />
+            <View style={[{ height: windowHeight }, styles.mainContainer]}>
+                <View style={{ width: logoWidth }}>
+                    <View
+                        style={[
+                            styles.tanifyLogoContainer,
+                            { height: logoHeight },
+                        ]}>
+                        <Image source={TanifyLogo} style={styles.tanifyLogo} />
+                    </View>
+                    <Text style={styles.temp}>
+                        {weatherData?.temperature >= 0 && '+'}
+                        {weatherData?.temperature}°C
+                    </Text>
+                </View>
+
+                <View style={styles.uv}>
+                    <UvIndex index={weatherData.uvIndex} />
+                </View>
+                <Text style={styles.slogan}>‘Nothing interesting happens’</Text>
+                <CircleView
+                    diameter={80}
+                    color={'#00cc7e'}
+                    style={styles.modeContainer}>
+                    <Text style={styles.mode}>SAFE</Text>
+                </CircleView>
+                <View style={styles.locationContainer}>
+                    <Text style={styles.location}>Tallinn, Estonia</Text>
+                </View>
             </View>
-            <Text style={styles.temp}>
-                {weatherData?.temperature >= 0 && '+'}
-                {weatherData?.temperature}°C
-            </Text>
-            <View style={styles.uv}>
-                <UvIndex index={weatherData.uvIndex} />
+
+            <View style={[{ height: windowHeight }, styles.secondContainer]}>
+                <CircleView
+                    diameter={180}
+                    color={'#00cc7e'}
+                    style={styles.uvStickerContainerStyle}>
+                    <Text style={styles.uvStickerStyle}>UV 0</Text>
+                </CircleView>
+                <View style={styles.uvInfoBoxContainerStyle}>
+                    <Text style={styles.uvInfoBoxStyle}>SAFE</Text>
+                </View>
+                <Text style={styles.uvInfo}>
+                    The sun's not interested in giving you a tan. We know it's
+                    hard, but just go out and do something else that's not
+                    tanning. Go on a date? Sudoku? It's up to you.
+                </Text>
             </View>
-            <Text style={styles.slogan}>'Nothing interesting happens'</Text>
-            <CircleView
-                diameter={100}
-                color={'#00cc7e'}
-                style={styles.modeContainer}>
-                <Text style={styles.mode}>SAFE</Text>
-            </CircleView>
-            <View style={styles.locationContainer}>
-                <Text style={styles.location}>Tallinn, Estonia</Text>
-            </View>
-            <CircleView
-                diameter={180}
-                color={'#00cc7e'}
-                style={styles.uvStickerContainerStyle}>
-                <Text style={styles.uvStickerStyle}>UV 0</Text>
-            </CircleView>
-            <View style={styles.uvInfoBoxContainerStyle}>
-                <Text style={styles.uvInfoBoxStyle}>SAFE</Text>
-            </View>
-            <Text style={styles.uvInfo}>
-                The sun's not interested in giving you a tan. We know it's hard,
-                but just go out and do something else that's not tanning. Go on
-                a date? Sudoku? It's up to you.
-            </Text>
         </ScrollView>
     );
 };
@@ -67,13 +90,16 @@ const styles = StyleSheet.create({
     scrollView: {
         backgroundColor: '#2b2b2b',
     },
-    contentContainer: {
+    mainContainer: {
         alignItems: 'center',
-        paddingTop: '10%',
-        paddingHorizontal: 20,
+        paddingTop: '20%',
+        paddingHorizontal: 30,
+    },
+    secondContainer: {
+        alignItems: 'center',
+        paddingHorizontal: 30,
     },
     tanifyLogoContainer: {
-        height: '10%',
         width: '100%',
     },
     tanifyLogo: {
@@ -87,15 +113,16 @@ const styles = StyleSheet.create({
         fontSize: 25,
         fontFamily: 'EuclidCircularB-Bold',
         alignSelf: 'flex-end',
+        marginTop: 2,
     },
     uv: {
         marginTop: '8%',
-        height: '17%',
+        height: '30%',
         width: '100%',
     },
     slogan: {
         color: 'white',
-        marginTop: '6%',
+        marginTop: '10%',
         fontSize: 20,
         fontFamily: 'EuclidCircularB-BoldItalic',
     },
@@ -111,31 +138,33 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
         paddingVertical: 8,
         marginTop: '7%',
+        borderRadius: 3,
     },
     location: {
         fontFamily: 'EuclidCircularB-Bold',
     },
     uvStickerContainerStyle: {
-        marginTop: 180,
+        marginTop: '20%',
     },
     uvStickerStyle: {
         fontFamily: 'EuclidCircularB-Bold',
-        fontSize: 45,
+        fontSize: 55,
         color: 'white',
     },
     uvInfoBoxContainerStyle: {
         backgroundColor: 'white',
-        paddingVertical: 4,
+        paddingVertical: 2,
         paddingHorizontal: 30,
+        width: 180,
         marginTop: 50,
+        alignItems: 'center',
     },
     uvInfoBoxStyle: {
         fontFamily: 'EuclidCircularB-Bold',
-        fontSize: 23,
+        fontSize: 25,
     },
     uvInfo: {
         color: 'white',
-        paddingHorizontal: 40,
         marginTop: 50,
         fontSize: 23,
         marginBottom: 180,
