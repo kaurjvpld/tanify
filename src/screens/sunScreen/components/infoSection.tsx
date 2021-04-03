6678a6a6a35daeaa2d8dab996af09b5c4a2982b3
Kaur Järvpõld <kaur@comodule.com>
2021-04-03 15:30:11 +0300

    info section responsive design

--- src/screens/sunScreen/components/infoSection.tsx
+++ src/screens/sunScreen/components/infoSection.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { circleViewColor, textColor } from '../../../util/colorUtil';
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { circleViewColor } from '../../../util/colorUtil';
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 import { StyleSheet, View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { Mode } from '../../../store/system/types';
@@ -8,30 +11,23 @@ import CircleView from '../../../components/circleView';
 import I18n from '../../../i18n/index';
 
 const InfoSection = () => {
-    const timeOfDay = useSelector((state) => state.system.timeOfDay);
     const mode = useSelector((state) => state.system.mode);
     const uv = useSelector((state) => state.system.uv);
 
     return (
         <View style={styles.container}>
             <CircleView
-                diameter={180}
+                diameter={hp('23%')}
                 color={circleViewColor(mode)}
                 style={styles.uvStickerContainerStyle}>
                 <Text style={styles.uvStickerStyle}>UV {uv}</Text>
             </CircleView>
             <View
                 style={[
-                    styles.uvInfoBoxContainerStyle,
+                    styles.modeContainer,
                     { backgroundColor: circleViewColor(mode) },
                 ]}>
-                <Text
-                    style={[
-                        styles.uvInfoBoxStyle,
-                        { color: textColor(timeOfDay) },
-                    ]}>
-                    {I18n.t(`mode.${mode}.title`)}
-                </Text>
+                <Text style={styles.mode}>{I18n.t(`mode.${mode}.title`)}</Text>
             </View>
             <Text style={styles.uvInfo}>{I18n.t(`mode.${mode}.info`)}</Text>
             {mode && mode !== Mode.Safe && (
@@ -63,28 +59,29 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
     },
     uvStickerContainerStyle: {
-        marginTop: '20%',
+        marginTop: hp('9%'),
     },
     uvStickerStyle: {
         fontFamily: 'EuclidCircularB-Bold',
-        fontSize: 55,
+        fontSize: hp('8%'),
         color: 'white',
     },
-    uvInfoBoxContainerStyle: {
+    modeContainer: {
         paddingVertical: 2,
         paddingHorizontal: 30,
-        width: 180,
-        marginTop: 50,
+        width: wp('55%'),
+        marginTop: hp('7%'),
         alignItems: 'center',
         borderRadius: 3,
     },
-    uvInfoBoxStyle: {
+    mode: {
         fontFamily: 'EuclidCircularB-Bold',
         fontSize: 25,
+        color: 'white',
     },
     uvInfo: {
         color: 'white',
-        marginTop: 50,
+        marginTop: hp('5%'),
         fontSize: hp('3.2%'),
         fontFamily: 'EuclidCircularB-Regular',
         alignSelf: 'flex-start',
